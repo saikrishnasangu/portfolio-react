1e6bc45ac45bde6f274712bdf3c7e8dde0c586f4
saikrishnasangu <saikrishna.sangu1@gmail.com>
2024-04-11 23:36:45 +0530

    Changes

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -48,7 +48,7 @@ const Contact=()=>{
         </motion.div>
         <motion.div className="item" variants={variants}>
             <h2>Address</h2>
-            <span>Hyderabad,Telangana, India</span>
+            <span>Hyderabad, Telangana, India</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
             <h2>Phone</h2>
